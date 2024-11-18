331a76c3141a4fe8c4e6acec16bc1534764122b8
TobiasAdler <tobias.adler2@stud.htw-dresden.de>
2024-11-18 18:44:14 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    <div id="app">
+      <h1>Character Picker</h1>
+      <div class="filters">
+        <label>
+          Wähle ein Spiel:
+          <select v-model="selectedGame">
+            <option v-for="game in uniqueGames" :key="game" :value="game">{{ game }}</option>
+          </select>
+        </label>
+        <button @click="pickRandomCharacter">Zufälligen Charakter wählen</button>
+      </div>
+      
+      <div v-if="selectedCharacter" class="character-display">
+        <h2>{{ selectedCharacter.name }}</h2>
+        <p>Element: {{ selectedCharacter.element }}</p>
+        <p>Typ: {{ selectedCharacter.type }}</p>
+        <img :src="selectedCharacter.img" alt="Character Image" />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import characters from './assets/characters.json';
+  
+  export default {
+    data() {
+      return {
+        characters,
+        selectedGame: null,
+        selectedCharacter: null,
+      };
+    },
+    computed: {
+      uniqueGames() {
+        return [...new Set(this.characters.map((char) => char.game))].sort((a, b) => a - b);
+      },
+    },
+    methods: {
+      pickRandomCharacter() {
+        // Filtere die Charaktere nach aufwärtskompatiblen Spielen
+        const filteredCharacters = this.characters.filter(
+          (char) => char.game <= this.selectedGame
+        );
+        if (filteredCharacters.length > 0) {
+          // Zufälligen Charakter auswählen
+          const randomIndex = Math.floor(Math.random() * filteredCharacters.length);
+          this.selectedCharacter = filteredCharacters[randomIndex];
+        } else {
+          alert("Keine Charaktere für das gewählte Spiel gefunden!");
+        }
+      },
+    },
+  };
+  </script>
+  
+  <style>
+  #app {
+    font-family: Arial, sans-serif;
+    text-align: center;
+    margin: 20px;
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  .filters {
+    margin: 20px;
+  }
+  .character-display img {
+    max-width: 200px;
+    height: auto;
+  }
+  </style>
+  
